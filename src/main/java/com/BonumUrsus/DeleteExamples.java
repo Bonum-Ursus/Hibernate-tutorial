7470c8f89b90a61ee2ec592ad1f791b2cfefa688
Bonum-Ursus <Viktor.Evtyukhin@mail.ru>
2020-02-21 18:19:02 +0500

    Instructor and InstructorDetail classes added

--- src/main/java/com/BonumUrsus/DeleteExamples.java
+++ src/main/java/com/BonumUrsus/DeleteExamples.java
@@ -4,25 +4,26 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class UpdateExample {
-
+public class DeleteExamples {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
-                .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Student.class)
                 .buildSessionFactory();
         Session session = factory.getCurrentSession();
         try{
             session.beginTransaction();
-            Student student = session.get(Student.class, 4);
-            student.setFirstName("Yennefer");
 
-//            session.createQuery("UPDATE Student set email='witcher.characters@gmal.com'")
-//                    .executeUpdate();
+            // Version 1
+            Student student = session.get(Student.class, 1);
+            session.delete(student);
+
+            // Version 2
+            session.createQuery("delete from Student where id=1").executeUpdate();
 
             session.getTransaction().commit();
         }finally {
             factory.close();
         }
+
     }
 }
